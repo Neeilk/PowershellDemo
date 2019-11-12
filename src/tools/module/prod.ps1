c77cb32c609ddc9af6e1f129c977649f68016ce2
Neel Kamal <neeilk@outlook.com>
2019-11-12 13:02:05 +0000

    updated wrappers

---
+++ src/tools/module/prod.ps1
@@ -0,0 +1,28 @@
+function Global:prompt {"PROD | DemoModule>"} 
+
+$Global:Staging = $false
+$Global:ActionParallelism = 3
+$Global:Timeout = 120
+$Global:NoPrompt = $false
+$Global:RunChecks = $true
+$Global:SlwIntegration = $false
+
+Import-Module DemoModule
+$Host.UI.RawUI.BackgroundColor = ($bckgrnd = 'Black')
+$Host.UI.RawUI.ForegroundColor = 'Green'
+$Host.PrivateData.ErrorForegroundColor = 'Red'
+$Host.PrivateData.ErrorBackgroundColor = $bckgrnd
+$Host.PrivateData.WarningForegroundColor = 'Magenta'
+$Host.PrivateData.WarningBackgroundColor = $bckgrnd
+$Host.PrivateData.DebugForegroundColor = 'Gray'
+$Host.PrivateData.DebugBackgroundColor = $bckgrnd
+$Host.PrivateData.VerboseForegroundColor = 'Yellow'
+$Host.PrivateData.VerboseBackgroundColor = $bckgrnd
+$Host.PrivateData.ProgressForegroundColor = 'Cyan'
+$Host.PrivateData.ProgressBackgroundColor = $bckgrnd
+[console]::ForegroundColor = "Green"
+[console]::BackgroundColor = "Black"
+Clear-Host
+Write-Host "PROD Values Set"
+Read-Host "Press Any Key to Continue.."
+Clear-Host
---
+++ src/tools/module/stg.ps1
@@ -0,0 +1,29 @@
+function Global:prompt {"STG | DemoModule>"} 
+
+$Global:Staging = $true
+$Global:ActionParallelism = 5
+$Global:Timeout = 30
+$Global:NoPrompt = $false
+$Global:RunChecks = $true
+$Global:SlwIntegration = $false
+
+Import-Module DemoModule
+$Host.UI.RawUI.BackgroundColor = ($bckgrnd = 'Gray')
+$Host.UI.RawUI.ForegroundColor = 'Blue'
+$Host.PrivateData.ErrorForegroundColor = 'Red'
+$Host.PrivateData.ErrorBackgroundColor = $bckgrnd
+$Host.PrivateData.WarningForegroundColor = 'Magenta'
+$Host.PrivateData.WarningBackgroundColor = $bckgrnd
+$Host.PrivateData.DebugForegroundColor = 'Black'
+$Host.PrivateData.DebugBackgroundColor = $bckgrnd
+$Host.PrivateData.VerboseForegroundColor = 'Yellow'
+$Host.PrivateData.VerboseBackgroundColor = $bckgrnd
+$Host.PrivateData.ProgressForegroundColor = 'Cyan'
+$Host.PrivateData.ProgressBackgroundColor = $bckgrnd
+[console]::ForegroundColor = "Blue"
+[console]::BackgroundColor = "Gray"
+Clear-Host
+Write-Host ""
+Write-Host "STAGING Values Set"
+Read-Host "Press Any Key to Continue.."
+Clear-Host
